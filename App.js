a22eb7b1e32a19221734d0f3778fe27a841c2430
Yiwei Zhang <happyzyw@umich.edu>
2020-05-22 21:57:28 -0500

    take a pic and upload

--- App.js
+++ App.js
@@ -1,75 +1,162 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native';
-import logo from './assets/splash.png'
+import { StyleSheet, Text, View ,TouchableOpacity,Platform, Image, } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing';
-import uploadToAnonymousFilesAsync from 'anonymous-files';
 
 
 
-export default function App() {
-    const [selectedImage, setSelectedImage] = React.useState(null);
-    let openImagePickerAsync = async () => {
-        let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
 
-        if (permissionResult.granted === false) {
-            alert("Permission to access camera roll is required!");
-            return;
-        }
-
-        let pickerResult = await ImagePicker.launchImageLibraryAsync();
-        if (pickerResult.cancelled === true) {
-            return;
-        }
-
-        setSelectedImage({ localUri: pickerResult.uri });
-    };
-
-
-    let openShareDialogAsync = async () => {
-        if (!(await Sharing.isAvailableAsync())) {
-            alert(`Uh oh, sharing isn't available on your platform`);
-            return;
-        }
-
-        Sharing.shareAsync(selectedImage.localUri);
-    };
-
-    if (selectedImage !== null) {
-        return (
-            <View style={styles.container}>
-                <Image
-                    source={{ uri: selectedImage.localUri }}
-                    style={styles.thumbnail}
-                />
-                <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-                    <Text style={styles.buttonText}>Share this photo</Text>
-                </TouchableOpacity>
-            </View>
-        );
+export default class App extends React.Component {
+    state = {
+        hasPermission: null,
+        cameraType: Camera.Constants.Type.back,
+        selectedImage: null,
     }
 
-    return (
-        <View style={styles.container}>
-            <Image source={logo} style={{width: 305, height: 159}} />
-            <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
-            <Text style={{color: '#888', fontSize: 18}}>
-            To share a photo from your phone with a friend, just press the button below!
-            </Text>
-            <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-                <Text style={styles.buttonText}>Pick a photo</Text>
-            </TouchableOpacity>
-        </View>
-    );
+
+    async componentDidMount() {
+        this.getPermissionAsync()
+    }
+
+    getPermissionAsync = async () => {
+        // Camera roll Permission
+        if (Platform.OS === 'ios') {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+            }
+        }
+        // Camera Permission
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasPermission: status === 'granted' });
+    }
+
+    handleCameraType=()=>{
+        const { cameraType } = this.state
+
+        this.setState({cameraType:
+                cameraType === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+        })
+    }
+
+    takePicture = async () => {
+        if (this.camera) {
+            let photo = await this.camera.takePictureAsync();
+            this.setState({selectedImage: photo.uri});
+        }
+    }
+
+    pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images
+        });
+        this.setState({selectedImage: result.uri});
+        console.log(result);
+    }
+
+
+
+    render(){
+        const { hasPermission } = this.state
+        if (hasPermission === null) {
+            return <View />;
+        } else if (hasPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+            if (this.state.selectedImage !== null) {
+                return (
+                    <View style={styles.container}>
+                        {/*<Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />*/}
+                        <Text style={styles.instructions}>
+                            To share a photo from your phone with a friend, just press the button below!
+                        </Text>
+
+                        <Image
+                            source={{ uri: this.state.selectedImage }}
+                            style={styles.thumbnail}
+                        />
+
+                        {/*<TouchableOpacity*/}
+                        {/*    onPress={()=>this.takePicture()}*/}
+                        {/*    style={{ backgroundColor: 'blue' }}>*/}
+                        {/*    <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>*/}
+                        {/*</TouchableOpacity>*/}
+                    </View>
+                );
+            }
+
+            return (
+                <View style={{ flex: 1 }}>
+                    <Camera style={{ flex: 1 }} type={this.state.cameraType}  ref={ref => {this.camera = ref}}>
+                        <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:30}}>
+                            <TouchableOpacity
+                                style={{
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                    backgroundColor: 'transparent'
+                                }}
+                                onPress={()=>this.pickImage()}>
+                                <Ionicons
+                                    name="ios-photos"
+                                    style={{ color: "#fff", fontSize: 40}}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                    backgroundColor: 'transparent',
+                                }}
+                                onPress={()=>this.takePicture()}
+                            >
+                                <FontAwesome
+                                    name="camera"
+                                    style={{ color: "#fff", fontSize: 40}}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                    backgroundColor: 'transparent',
+                                }}
+                                onPress={()=>this.handleCameraType()}
+                            >
+                                <MaterialCommunityIcons
+                                    name="camera-switch"
+                                    style={{ color: "#fff", fontSize: 40}}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
+            );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 305,
+        height: 159,
+        marginBottom: 20,
+    },
+    instructions: {
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+        marginBottom: 10,
+    },
     button: {
         backgroundColor: "blue",
         padding: 20,
